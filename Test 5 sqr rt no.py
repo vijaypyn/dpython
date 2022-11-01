338928cf2a2fb8174080610c1c83d218b0746586
vijaypyn <117083499+vijaypyn@users.noreply.github.com>
2022-11-01 18:45:42 +0530

    Add files via upload

---
+++ Test 5 sqr rt no.py
@@ -0,0 +1,7 @@
+#write a python program which (Task 1) takes a number and squares it and then (Task 2) take the square root of the result you get from the task(1)
+import math
+n= int(input("enter a no: "))
+x= (n**2)
+print(x)
+y= math.sqrt(x)
+print(y)
---
+++ Test 6 reverse name.py
@@ -0,0 +1,8 @@
+#Write a Python function to reverses a string if it's length is a multiple of 4.
+str1=input("enter the text: ")
+def reverse_string(str1):
+        if len(str1) % 4 == 0:
+                return ''.join(reversed(str1))
+                print(reverse_string('str1'))
+                return (str1)
+                print(reverse_string('Testing'))

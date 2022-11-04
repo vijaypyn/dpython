6428d7e39b525d28d1229c4eff72bbc31c855539
vijaypyn <117083499+vijaypyn@users.noreply.github.com>
2022-11-04 10:56:08 +0530

    Update Test 6 reverse name.py

--- Test 6 reverse name.py
+++ Test 6 reverse name.py
@@ -1,8 +1,7 @@
 #Write a Python function to reverses a string if it's length is a multiple of 4.
-str1=input("enter the text: ")
-def reverse_string(str1):
-        if len(str1) % 4 == 0:
-                return ''.join(reversed(str1))
-                print(reverse_string('str1'))
-                return (str1)
-                print(reverse_string('Testing'))
+name=input("enter a name:")
+
+if(len(name)%4==0):
+    print(name[::-1])
+else:
+    print("cant")  
